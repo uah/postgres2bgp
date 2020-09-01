ab8ec0fe21174c5c527aacfaf14d9f61e96ef673
Hunter L Fuller - hf0002 <hf0002@uah.edu>
2020-08-31 20:46:32 -0500

    Summarize prefixes, and send only what has changed.
    Next i will allow subscriptions.

--- postgres2bgp.py
+++ postgres2bgp.py
@@ -4,6 +4,7 @@ import sys
 import json
 import psycopg2
 import time
+import netaddr
 
 def eprint(*args, **kwargs):
     print("[postgres2bgp]", *args, file=sys.stderr, **kwargs)
@@ -18,15 +19,34 @@ with psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(con
 
 eprint("Connected to database on", config['db_host'])
 
-eprint("About to do initial prefix load.")
-
-cursor.execute("select distinct srcaddr from ti_event where now() > start_stamp and now() < end_stamp order by srcaddr;")
-eprint("Loading", cursor.rowcount, "prefixes")
-for row in cursor:
-    ip = row[0]
-    #TODO: summarize
-    cmd = "announce route {}/32 next-hop 192.18.66.66 community [no-export {}:{}]".format(ip, config['asn'], config['bh_community'])
-    print(cmd)
+prefixes_loaded = []
 
 while True:
-    time.sleep(1000)
+    eprint("Going to insert/withdraw prefixes now.")
+
+    prefixes_to_load = []
+    cursor.execute("select distinct srcaddr from ti_event where now() > start_stamp and now() < end_stamp order by srcaddr;")
+    for row in cursor:
+        prefixes_to_load.append(netaddr.IPNetwork(row[0]))
+
+    eprint("The original IP list has", len(prefixes_to_load), "prefixes")
+    prefixes_to_load = netaddr.cidr_merge(prefixes_to_load)
+    eprint("This summarizes to", len(prefixes_to_load), "prefixes")
+
+    prefixes_to_announce = [prefix for prefix in prefixes_to_load if prefix not in prefixes_loaded]
+    prefixes_to_withdraw = [prefix for prefix in prefixes_loaded if prefix not in prefixes_to_load]
+
+    eprint("Going to announce", len(prefixes_to_announce), "prefixes and withdraw", len(prefixes_to_withdraw), "prefixes")
+
+    for prefix in prefixes_to_announce:
+        cmd = "announce route {} next-hop 192.18.66.66 community [no-export {}:{}]".format(prefix, config['asn'], config['bh_community'])
+        print(cmd)
+        
+    for prefix in prefixes_to_withdraw:
+        cmd = "withdraw route {} next-hop 192.18.66.66 community [no-export {}:{}]".format(prefix, config['asn'], config['bh_community'])
+        print(cmd)
+
+    eprint("updates have been sent to exabgp.")
+    prefixes_loaded = prefixes_to_load
+
+    time.sleep(15*60) #It's only going to be like this til we have subscriptions
